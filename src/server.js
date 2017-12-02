3caa6b459d2c723be8cb603b59c5c1b6abf7a56e
dean <dean@deannolan.co.uk>
2017-12-02 15:51:20 +0000

    added config

--- src/server.js
+++ src/server.js
@@ -1,1 +1,12 @@
-console.log('server');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+var config = require('config');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(config.get('PORT'));
+console.log(`server listenging on port ${config.get('PORT')}`);
