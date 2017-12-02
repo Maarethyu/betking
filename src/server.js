586d413b02ff665a170a444162c3b1b30755da01
dean <dean@deannolan.co.uk>
2017-12-02 16:25:50 +0000

    setup server to serve front end html contained in a different repository

--- src/server.js
+++ src/server.js
@@ -1,12 +1,20 @@
+const config = require('config');
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-var config = require('config');
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(cors());
+/* the front end code is in another repository and developed independantly 
+  we serve the built front end code from here
+  this allows developers to work on front end code without the security risk of them having access to how
+  the backend works.
+  It also allows us to have register/login code separate from front end while not worrying about different domains
+  TODO - is this a bad way to do this?
+*/
+app.use(express.static('@/../../client/dist/'));
+
+app.get('/', function (req, res) {
+  res.sendFile('index.html'); // fron the front end folder
+});
 
 app.listen(config.get('PORT'));
 console.log(`server listenging on port ${config.get('PORT')}`);
