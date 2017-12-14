1152480edf3a17c16b3fd7d2b6c389289aef9b8b
dean <dean@deannolan.co.uk>
2017-12-14 20:41:34 +0000

    removed some todo comments and added them to git issues

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -10,9 +10,6 @@ const db = require('../db');
 // context.response.redirect(redirecturl)
 // };
 
-// add recaptcha here
-// rate limit
-// csrf
 router.post('/register', async function (req, res, next) {
   req.check('password', 'Invalid Password').exists()
     .isLength({min: 6, max: 50});
@@ -28,40 +25,32 @@ router.post('/register', async function (req, res, next) {
     .trim()
     .isLength({min: 2, max: 20})
     .matches(/^[a-z0-9_]+$/i) // name contains invalid characters
-    // .not()
-    // .matches(/[_]{2,}/i) // name contains consecutive underscores
     .not()
     .matches(/^[_]|[_]$/i); // name starts or ends with underscores
-  // .custom((value, { req }) => db.isUserNameAlreadyTaken(req.body.username) === false);
-    
-  // not blacklisted username (admin etc)
 
   const errors = req.validationErrors();
   if (errors) {
     return res.status(422).json({errors});
   }
 
+  // TODO can this be a custom express-validator?
   const emailExists = await db.isEmailAlreadyTaken(req.body.email);
   if (emailExists) {
     return res.status(422).json({error: 'email already exists'});
   }
+
+  // TODO can this be a custom express-validator?
   const userNameExists = await db.isUserNameAlreadyTaken(req.body.username);
   if (userNameExists) {
     return res.status(422).json({error: 'username already exists'});
   }
 
-  // const appId = 0; // BetKing
-  // get affiliate id
-  // const affiliateId = null;
-
   const hash = await bcrypt.hash(req.body.password, 10); 
   
-  const user = await db.createUser(req.body.username, hash, req.body.email); // ipAddress, appid, affiliateid);
+  const user = await db.createUser(req.body.username, hash, req.body.email); // ipAddress, appid = 0);
   if (user) {
     // await completeLogin(this, user.id, false, 'Registration a success');
     res.json(user);
-    // if email supplied, send confirm email ?
-    // send welcome email?
   } else {
     res.status(422)
       .end();
