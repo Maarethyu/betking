db3bdb1410c9a82c18d0d38be95248047e379875
dean <dean@deannolan.co.uk>
2017-12-18 01:10:06 +0000

    fix middleware

--- src/server.js
+++ src/server.js
@@ -41,7 +41,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(expressValidator());
 
-app.use(mw.attachCurrentUserToRequest());
+app.use(mw.attachCurrentUserToRequest);
 
 const router = express.Router();
 router.use('', require('./routes/index')); 
