0ff86fc0a14cfeac73ded9395e85827af4d493da
Naisheel Verdhan <nverdhan@gmail.com>
2018-01-02 17:44:01 +0530

    add route for fetching user-profile

--- src/routes/account.js
+++ src/routes/account.js
@@ -8,6 +8,17 @@ router.use(mw.requireLoggedIn);
 router.post('/logout', async function (req, res, next) {
   await db.logoutSession(req.currentUser.id, req.cookies.session);
   // return what?
-});  
+});
+
+router.get('/me', async function (req, res, next) {
+    res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+    res.json({
+        id: req.currentUser.id,
+        username: req.currentUser.username,
+        email: req.currentUser.email,
+        isEmailVerified: req.currentUser.email_verified,
+        dateJoined: req.currentUser.date_joined
+    });
+});
 
 module.exports = router;
