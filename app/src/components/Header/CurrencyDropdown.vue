15a1e5d26ba723aa4ee845e2e3f44b41355ca4e1
Naisheel Verdhan <nverdhan@gmail.com>
2018-01-30 19:53:40 +0530

    correct html tags in header

--- app/src/components/Header/CurrencyDropdown.vue
+++ app/src/components/Header/CurrencyDropdown.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class='currency-dropdown'>
-    <b-nav-item-dropdown left class='text-left'>
-      <template slot="button-content">
-        <span class="d-none d-sm-inline-block d-md-inline-block d-lg-none d-xl-inline-block currency-dropdown__balance">
-          BALANCE: {{balance}}
-        </span>
-        <span><img src='/static/img/main/btc.png' width='13' height='13'></span>
-      </template>
-      <b-dropdown-item @click="currency = '(B)'; balance=$root.state.user.balance.btc"><i class="fa fa-btc"></i> Bitcoin</b-dropdown-item>
-      <b-dropdown-item @click="currency = '(L)'; balance=$root.state.user.balance.ltc"><i class="fa fa-btc"></i> Ethereum</b-dropdown-item>
-      <b-dropdown-item @click="currency = '(E)'; balance=$root.state.user.balance.eth"><i class="fa fa-btc"></i> Litecoin</b-dropdown-item>
-      <b-dropdown-item @click="currency = '(B)'; balance=$root.state.user.balance.bkb"><i class="fa fa-btc"></i> BetKing BKB</b-dropdown-item>
-    </b-nav-item-dropdown>
-  </div>
+  <b-nav-item-dropdown left class='text-left currency-dropdown'>
+    <template slot="button-content">
+      <span class="d-none d-sm-inline-block d-md-inline-block d-lg-none d-xl-inline-block currency-dropdown__balance">
+        BALANCE: {{balance}}
+      </span>
+      <span><img src='/static/img/main/btc.png' width='13' height='13'></span>
+    </template>
+    <b-dropdown-item @click="currency = '(B)'; balance=$root.state.user.balance.btc"><i class="fa fa-btc"></i> Bitcoin</b-dropdown-item>
+    <b-dropdown-item @click="currency = '(L)'; balance=$root.state.user.balance.ltc"><i class="fa fa-btc"></i> Ethereum</b-dropdown-item>
+    <b-dropdown-item @click="currency = '(E)'; balance=$root.state.user.balance.eth"><i class="fa fa-btc"></i> Litecoin</b-dropdown-item>
+    <b-dropdown-item @click="currency = '(B)'; balance=$root.state.user.balance.bkb"><i class="fa fa-btc"></i> BetKing BKB</b-dropdown-item>
+  </b-nav-item-dropdown>
 </template>
 
 <style lang="scss">
