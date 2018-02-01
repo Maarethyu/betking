ac55de1490fa42b3f033b6993c79d7ed23bf9c66
Naisheel Verdhan <nverdhan@gmail.com>
2018-02-01 17:54:52 +0530

    add currency icons

--- app/src/components/Header/CurrencyDropdown.vue
+++ app/src/components/Header/CurrencyDropdown.vue
@@ -4,9 +4,12 @@
       <span class="d-none d-sm-inline-block d-md-inline-block d-lg-none d-xl-inline-block currency-dropdown__balance">
         BALANCE: {{addCommas(formatAmount(activeCurrencyBalance, activeCurrency))}}
       </span>
-      <span><img src='/static/img/main/btc.png' width='13' height='13'></span>
+      <span><CurrencyIcon :value="activeCurrency" :width="13" /></span>
     </template>
-    <b-dropdown-item v-for="currency in currencies" :key="currency.symbol" @click="setActiveCurrency(currency.value)" balance="currency.balance"><i class="fa fa-btc"></i>{{currency.name}}</b-dropdown-item>
+    <b-dropdown-item v-for="currency in currencies" :key="currency.symbol"
+      @click="setActiveCurrency(currency.value)" balance="currency.balance">
+        <CurrencyIcon :value="currency.value" />{{currency.name}}
+    </b-dropdown-item>
   </b-nav-item-dropdown>
 </template>
 
@@ -49,18 +52,19 @@
 
 
 <script>
-  import {mapGetters} from 'vuex';
-
-  import {addCommas, formatAmount} from 'src/helpers';
-
   import bNavItemDropdown from 'bootstrap-vue/es/components/nav/nav-item-dropdown';
   import bDropdownItem from 'bootstrap-vue/es/components/dropdown/dropdown-item';
+
+  import {mapGetters} from 'vuex';
+  import {addCommas, formatAmount} from 'src/helpers';
+  import CurrencyIcon from 'components/CurrencyIcon';
 
   export default {
     name: 'currency-dropdown',
     components: {
       'b-nav-item-dropdown': bNavItemDropdown,
-      'b-dropdown-item': bDropdownItem
+      'b-dropdown-item': bDropdownItem,
+      CurrencyIcon
     },
     computed: {
       ...mapGetters({
