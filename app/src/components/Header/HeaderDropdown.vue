071674ea17d843560500a6f23121caa9dd0d9bd8
Naisheel Verdhan <nverdhan@gmail.com>
2018-02-02 14:10:12 +0530

    fix account dropdown: avatar-position and width (fix #13)

--- app/src/components/Header/HeaderDropdown.vue
+++ app/src/components/Header/HeaderDropdown.vue
@@ -1,8 +1,9 @@
 <template>
-  <b-nav-item-dropdown right>
+  <b-nav-item-dropdown right class="account-dropdown">
     <template slot="button-content">
       <Avatar :username="username" :size="30" :backgroundColor="'#d53939'"
-        :color="'#fff'" :customStyle="{display: 'inline-block', position: 'relative', top: -3}"></Avatar>
+        :color="'#fff'"
+        :customStyle="{display: 'inline-block', position: 'relative', 'margin-right': '5px'}"></Avatar>
       <span class="d-md-down-none">{{ username }}</span>
     </template>
     <b-dropdown-header tag="div" class="text-center"><strong>Profile</strong></b-dropdown-header>
@@ -17,15 +18,26 @@
     <b-dropdown-item @click="logout"><i class="fa fa-lock"></i> Logout</b-dropdown-item>
   </b-nav-item-dropdown>
 </template>
+
 <style lang="scss">
-  .dropdown-toggle::after {
-    margin-left: 0.5em;
-    border-top: 0.5em solid;
-    border-right: 0.5em solid transparent;
-    border-left: 0.5em solid transparent;
-    vertical-align: 0;
+  .navbar-nav .account-dropdown {
+    .dropdown-toggle {
+      padding: 0 10px;
+
+      &::after {
+        margin-left: 0.5em;
+        border-top: 0.5em solid;
+        border-right: 0.5em solid transparent;
+        border-left: 0.5em solid transparent;
+        vertical-align: 0;
+      }
+    }
+    .dropdown-menu {
+      padding: 10px!important;
+    }
   }
 </style>
+
 <script>
   import bNavItemDropdown from 'bootstrap-vue/es/components/nav/nav-item-dropdown';
   import bDropdownHeader from 'bootstrap-vue/es/components/dropdown/dropdown-header';
