1b1b82d74989be9d5db4d32e774cc86922ae6f0a
g1aurav <gauravkumar0154@gmail.com>
2018-02-06 15:19:20 +0530

    update qr img size at deposit dialog

--- app/src/components/Modals/DepositModal.vue
+++ app/src/components/Modals/DepositModal.vue
@@ -12,7 +12,7 @@
         </b-row>
         <b-row>
           <b-col class="text-center">
-            <img v-if="depositAddressQr && depositAddressQr.length > 0" :src="depositAddressQr" />
+            <img class="qr" v-if="depositAddressQr && depositAddressQr.length > 0" :src="depositAddressQr" />
           </b-col>
         </b-row>
         <b-row>
@@ -35,7 +35,11 @@
 
   </b-modal>
 </template>
-
+<style>
+  img.qr {
+    width: 300px;
+  }
+</style>
 <script>
   import bModal from 'bootstrap-vue/es/components/modal/modal';
   import bContainer from 'bootstrap-vue/es/components/layout/container';
