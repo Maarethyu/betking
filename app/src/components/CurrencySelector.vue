2effccfc11043516945abdfa41ff2225d1a67ef1
Naisheel Verdhan <nverdhan@gmail.com>
2018-02-06 17:13:00 +0530

    fix currency dropdown, fix scrollbar on wd whitelist

--- app/src/components/CurrencySelector.vue
+++ app/src/components/CurrencySelector.vue
@@ -5,7 +5,9 @@
     </template>
 
     <b-dropdown-item v-for="currency of currencies" :key="currency.value" @click="currencyChanged(currency)">
-      {{ currency.name }} <CurrencyIcon :className="'float-right'" :value="currency.value" :width="15" />
+      <span class="float-left">{{ currency.name }}</span>
+      <CurrencyIcon :className="'float-right'" :value="currency.value" :width="15" />
+      <div class="clearfix" />
     </b-dropdown-item>
   </b-dropdown>
 </template>
