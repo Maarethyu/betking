961260106ab16af33b77facecc6789e212c36532
Naisheel Verdhan <nverdhan@gmail.com>
2018-02-07 17:32:31 +0530

    fix #33: copy to clipboard fix

--- app/src/components/CopyToClipboard.vue
+++ app/src/components/CopyToClipboard.vue
@@ -7,35 +7,52 @@
       v-if="copyVisible"
       v-bind:disabled="typeof text === 'undefined' || text.toString().length === 0"
       class="btn"
-      v-bind:class="{'btn-success': copyText === 'Copied!', 'btn-danger': copyText === 'Error!', 'btn-default': copyText === 'Copy'}"
+      v-bind:class="{'btn-success': copyText === 'Copied!', 'btn-danger': copyText === 'Error!', 'btn-dark': copyText === 'Copy'}"
       v-on:click="copyToClipboard">
       {{ copyText }}
     </button>
+    <textarea ref="textarea" v-model="text">
+    </textarea>
   </div>
 </template>
 
-<style>
+<style lang="scss">
   .copy-to-clipboard {
     max-width: 100%;
     overflow: hidden;
     position: relative;
-  }
-  .copy-to-clipboard .copy-text.form-control {
-    border-top-right-radius: 0;
-    border-bottom-right-radius: 0;
-    overflow: hidden;
-    padding-right: 78px;
-    width: 100%;
-  }
-  .copy-to-clipboard .copy-text.form-control.copy-invisible {
-    padding-right: 15px;
-  }
-  .copy-to-clipboard .btn {
-    position: absolute;
-    top: 0;
-    right: 0;
-    border-top-left-radius: 0;
-    border-bottom-left-radius: 0;
+
+    textarea {
+      box-shadow: none;
+      outline: none;
+      border: none;
+      width: 2px;
+      height: 2px;
+      background: transparent;
+      position: fixed;
+      top: 0;
+      left: 0;
+    }
+
+    .copy-text.form-control {
+      border-top-right-radius: 0;
+      border-bottom-right-radius: 0;
+      overflow: hidden;
+      padding-right: 78px;
+      width: 100%;
+    }
+
+    .copy-text.form-control.copy-invisible {
+      padding-right: 15px;
+    }
+
+    .btn {
+      position: absolute;
+      top: 0;
+      right: 0;
+      border-top-left-radius: 0;
+      border-bottom-left-radius: 0;
+    }
   }
 </style>
 <script>
@@ -60,29 +77,15 @@
         }, 3000);
       },
       copyTextToClipboard (text) {
-        const textArea = document.createElement('textarea');
-        textArea.style.position = 'fixed';
-        textArea.style.top = 0;
-        textArea.style.left = 0;
-        textArea.style.width = '2em';
-        textArea.style.height = '2em';
-        textArea.style.padding = 0;
-        textArea.style.border = 'none';
-        textArea.style.outline = 'none';
-        textArea.style.boxShadow = 'none';
-        textArea.style.background = 'transparent';
-        textArea.value = text;
-        document.body.appendChild(textArea);
-        textArea.select();
         let msg;
         try {
+          // console.log(this.$refs.textarea);
+          this.$refs.textarea.select();
           const successful = document.execCommand('copy');
           msg = successful ? 'Copied!' : 'Error!';
-          return msg;
         } catch (err) {
           msg = 'Error!';
         }
-        document.body.removeChild(textArea);
         return msg;
       },
       copyToClipboard () {
